ccd931bee87b0c482568ee129ce3154cc26f1afa
Utibe-Abasi Akpan <utyemma@gmail.com>
2022-06-17 15:12:30 +0100

    Meal Controller for MEals

--- app/Models/Meal/MealCategory.php
+++ app/Models/Meal/MealCategory.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class MealCategory extends Model
-{
+class MealCategory extends Model {
     use HasFactory, SoftDeletes;
 
     protected $primaryKey = 'unique_id';
