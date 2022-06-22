f289fe98aee5f52cd9e086e2c48299f92101950b
Utibe-Abasi Akpan <utyemma@gmail.com>
2022-06-22 13:09:27 +0100

    Media Controller

--- app/Http/Controllers/Api/MediaController.php
+++ app/Http/Controllers/Api/MediaController.php
@@ -12,6 +12,10 @@ class MediaController extends Controller{
     use Generics, ReturnTemplate;
 
     function upload(Request $request){
+        return Response::json([
+            'file' => $request->file('media')
+        ]);
+
         $file = $this->uploadFileHandler($request, 'file', $request->folder);
 
         return Response::json([
